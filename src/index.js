3d80e7e5f29cf2544edee6abd99765cc944514e6
snclucas <simon@htpl.us>
2016-01-23 02:48:21 +0000

    Added strangle and spread strategies + tests

--- src/index.js
+++ src/index.js
@@ -1,26 +1,64 @@
 module.exports = {
 
-  single: function single(buyOrSell, callOrPut, underlyingPrice, strike, premium, priceRange) {
+  single: function single(buyOrSell, callOrPut, strike, premium, underlyingPrice, priceRange) {
     return {
       strategy: 'Single',
-      payoff: singleCalc(buyOrSell, callOrPut, underlyingPrice, strike, premium, priceRange),
+      type: callOrPut,
+      side: buyOrSell,
+      strike: strike,
+      underlying_price: underlyingPrice,
+      premium: premium,
+      payoff: singleOptionCalc(buyOrSell, callOrPut, strike, premium, underlyingPrice, priceRange),
     };
   },
-  straddle: function straddle(buyOrSell, underlyingPrice, strike, premium, priceRange) {
+  straddle: function straddle(buyOrSell, strike, callpremium, putpremium, underlyingPrice, priceRange) {
     return {
       strategy: 'Straddle',
-      payoff: straddleCalc(buyOrSell, underlyingPrice, strike, premium, priceRange),
+      side: buyOrSell,
+      strike: strike,
+      underlying_price: underlyingPrice,
+      callpremium: callpremium,
+      putpremium: putpremium,
+      payoff: twoOptionCalc(buyOrSell, "call", strike, callpremium, buyOrSell, "put", strike, putpremium, underlyingPrice, priceRange),
+    };
+  },
+  strangle: function strangle(buyOrSell, callstrike, putstrike, callpremium, putpremium, underlyingPrice, priceRange) {
+    return {
+      strategy: 'Strangle',
+      side: buyOrSell,
+      callstrike: callstrike,
+      putstrike: putstrike,
+      underlying_price: underlyingPrice,
+      callpremium: callpremium,
+      putpremium: putpremium,
+      payoff: twoOptionCalc(buyOrSell, "call", callstrike, callpremium, buyOrSell, "put", putstrike, putpremium, underlyingPrice, priceRange),
+    };
+  },
+  spread: function spread(buyOrSell1, callOrPut1, strike1, premium1, buyOrSell2, callOrPut2, strike2, premium2, underlyingPrice, priceRange) {
+    return {
+      strategy: 'Spread',
+      buyOrSell1: buyOrSell1,
+      buyOrSell2: buyOrSell2,
+      callOrPut1: callOrPut1,
+      callOrPut2: callOrPut2,
+      strike1: strike1,
+      strike2: strike2,
+      underlying_price: underlyingPrice,
+      premium1: premium1,
+      premium2: premium2,
+      payoff: twoOptionCalc(buyOrSell1, callOrPut1, strike1, premium1, buyOrSell2, callOrPut2, strike2, premium2, underlyingPrice, priceRange),
     };
   }
 };
 
 
 
-function straddleCalc(buyOrSell, underlyingPrice, strike, premium, priceRange) {
+
+function twoOptionCalc(buyOrSell1, side1, strike1, premium1, buyOrSell2, side2, strike2, premium2, underlyingPrice, priceRange) {
   var payoff = [];
   
-  var callOptionPayoff = singleCalc(buyOrSell, 'call', underlyingPrice, strike, premium, priceRange);
-  var putOptionPayoff = singleCalc(buyOrSell, 'put', underlyingPrice, strike, premium, priceRange);
+  var callOptionPayoff = singleOptionCalc(buyOrSell1, side1, strike1, premium1, underlyingPrice, priceRange);
+  var putOptionPayoff = singleOptionCalc(buyOrSell2, side2, strike2, premium2, underlyingPrice, priceRange);
   
   for (var i = 0; i < callOptionPayoff.length; i += 1)
     payoff.push(callOptionPayoff[i] + putOptionPayoff[i]);
@@ -28,10 +66,8 @@ function straddleCalc(buyOrSell, underlyingPrice, strike, premium, priceRange) {
 }
 
 
-
-function singleCalc(buyOrSell, callOrPut, underlyingPrice, strike, premium, priceRange) {
+function singleOptionCalc(buyOrSell, callOrPut, strike, premium, underlyingPrice, priceRange) {
     var side = (buyOrSell.toUpperCase() === 'BUY') ? 1 : -1;
-  
     var payoff = [];
   
     if (callOrPut.toUpperCase() === 'CALL')
--- test/index.test.js
+++ test/index.test.js
@@ -2,16 +2,59 @@ var expect = require('chai').expect;
 var optionPayoff = require('../src/index');
 
 describe('option-payoff', function() {
+  
   describe('single', function() {
-    it("Single payoff should look like {strategy: 'Single',payoff:[-15.2,-15.2,-15.2,-13.2,-11.2,-9.2,-5.199999999999999,-0.1999999999999993,9.8]}", function() {
-      var expected = {strategy: 'Single',payoff:[-15.2,-15.2,-15.2,-13.2,-11.2,-9.2,-5.199999999999999,-0.1999999999999993,9.8]};
-      expect(optionPayoff.single('buy', 'call',51.34,35,15.2,[30,33,35,37,39,41,45,50,60])).to.eql(expected);
+    it("Single buy call should have payoff:[-1,-1,-1,0,1]", function() {
+      var expected = {payoff:[-1,-1,-1,0,1]};
+      expect(optionPayoff.single('buy', 'call',3,1,3,[1,2,3,4,5]).payoff).to.eql(expected.payoff);
+    });
+    it("Single buy put should have payoff:[1,0,-1,-1,-1]", function() {
+      var expected = {payoff:[1,0,-1,-1,-1]};
+      expect(optionPayoff.single('buy', 'put',3,1,3,[1,2,3,4,5]).payoff).to.eql(expected.payoff);
+    });
+    
+    it("Single sell call should have payoff:[1,1,1,0,-1]", function() {
+      var expected = {payoff:[1,1,1,-0,-1]};
+      expect(optionPayoff.single('sell', 'call',3,1,3,[1,2,3,4,5]).payoff).to.eql(expected.payoff);
+    });
+    it("Single sell put should have payoff:[-1,0,1,1,1]", function() {
+      var expected = {payoff:[-1,-0,1,1,1]};
+      expect(optionPayoff.single('sell', 'put',3,1,3,[1,2,3,4,5]).payoff).to.eql(expected.payoff);
     });
   });
+  
   describe('straddle', function() {
-    it("Straddle payoff should look like {strategy: 'Straddle',payoff:[-25.4,-28.4,-30.4,-28.4,-26.4,-24.4,-20.4,-15.399999999999999,-5.399999999999999]}", function() {
-      var expected = {strategy: 'Straddle',payoff:[-25.4,-28.4,-30.4,-28.4,-26.4,-24.4,-20.4,-15.399999999999999,-5.399999999999999]};
-      expect(optionPayoff.straddle('buy', 51.34,35,15.2,[30,33,35,37,39,41,45,50,60])).to.eql(expected);
+    it("Straddle buy payoff should have payoff:[0,-1,-2,-1,0]", function() {
+      var expected = {payoff:[0,-1,-2,-1,0]};
+      expect(optionPayoff.straddle('buy', 3,1,1,3,[1,2,3,4,5]).payoff).to.eql(expected.payoff);
+    });
+    it("Straddle sell payoff should have payoff:[0,1,2,1,0]", function() {
+      var expected = {payoff:[0,1,2,1,0]};
+      expect(optionPayoff.straddle('sell', 3,1,1,3,[1,2,3,4,5]).payoff).to.eql(expected.payoff);
     });
   });
+  
+  describe('strangle', function() {
+    it("Strangle buy payoff should have payoff:[1,0,-1,-1,0,1]", function() {
+      var expected = {payoff:[1,0,-1,-1,0,1]};
+      expect(optionPayoff.strangle('buy', 3,4,1,1,3,[1,2,3,4,5,6]).payoff).to.eql(expected.payoff);
+    });
+    it("Strangle sell payoff should have payoff:[-1,0,1,1,0,-1]", function() {
+      var expected = {payoff:[-1,0,1,1,0,-1]};
+      expect(optionPayoff.strangle('sell', 3,4,1,1,3,[1,2,3,4,5,6]).payoff).to.eql(expected.payoff);
+    });
+  });
+  // spread(buyOrSell1, side1, strike1, premium1, buyOrSell2, side2, strike2, premium2, underlyingPrice, priceRange) 
+  
+   describe('spread', function() {
+    it("Spread buy payoff should have payoff:[-2,-2,-1,0,1,2,2,2,2]", function() {
+      var expected = {payoff:[-2,-2,-1,0,1,2,2,2,2]};
+      expect(optionPayoff.spread('buy', 'call', 2,1,'sell','call',6,-1,4,[1,2,3,4,5,6,7,8,9]).payoff).to.eql(expected.payoff);
+    });
+    it("Spread sell payoff should have payoff:[-6,-6,-6,-5,-4,-3,-2,-2,-2]", function() {
+      var expected = {payoff:[-6,-6,-6,-5,-4,-3,-2,-2,-2]};
+      expect(optionPayoff.spread('buy', 'put', 3,1,'sell','put',7,-1,5,[1,2,3,4,5,6,7,8,9]).payoff).to.eql(expected.payoff);
+    });
+  });
+  
 });
